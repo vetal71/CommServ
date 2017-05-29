873b542298ecf6c332addda7e3f69fb49ac6d6e0
vetal71 <vetalkolesnik@gmail.com>
2017-05-29 12:52:51 +0300

    Обновление модели данных

--- Model/20170527-01.sql
+++ Model/20170527-01.sql
@@ -16,16 +16,16 @@ IF EXISTS (SELECT
       ON (o.id = r.constid
       AND o.type = 'F')
     WHERE r.fkeyid = OBJECT_ID('dbo.Counters')
-    AND o.name = 'FK_COUNTERS_FK_COUNTE_CONSUMER')
+    AND o.name = 'FK_COUNTERS_CONSUMERID')
   ALTER TABLE dbo.Counters
-  DROP CONSTRAINT FK_COUNTERS_FK_COUNTE_CONSUMER
+  DROP CONSTRAINT FK_COUNTERS_CONSUMERID
 GO
 IF @@error <> 0
   AND @@trancount > 0
   ROLLBACK TRANSACTION
 GO
 IF @@error = 0
-  PRINT 'Drop FK_COUNTERS_FK_COUNTE_CONSUMER'
+  PRINT 'Drop FK_COUNTERS_CONSUMERID'
 
 IF EXISTS (SELECT
       1
@@ -34,34 +34,16 @@ IF EXISTS (SELECT
       ON (o.id = r.constid
       AND o.type = 'F')
     WHERE r.fkeyid = OBJECT_ID('dbo.Counters')
-    AND o.name = 'FK_COUNTERS_FK_COUNTE_COUNTERK')
+    AND o.name = 'FK_COUNTERS_COUNTERKIND')
   ALTER TABLE dbo.Counters
-  DROP CONSTRAINT FK_COUNTERS_FK_COUNTE_COUNTERK
+  DROP CONSTRAINT FK_COUNTERS_COUNTERKIND
 GO
 IF @@error <> 0
   AND @@trancount > 0
   ROLLBACK TRANSACTION
 GO
 IF @@error = 0
-  PRINT 'Drop FK_COUNTERS_FK_COUNTE_COUNTERK'
-
-IF EXISTS (SELECT
-      1
-    FROM sys.sysreferences r
-    JOIN sys.sysobjects o
-      ON (o.id = r.constid
-      AND o.type = 'F')
-    WHERE r.fkeyid = OBJECT_ID('dbo.GroupCounters')
-    AND o.name = 'FK_GROUPCOU_FK_GC_COU_COUNTERK')
-  ALTER TABLE dbo.GroupCounters
-  DROP CONSTRAINT FK_GROUPCOU_FK_GC_COU_COUNTERK
-GO
-IF @@error <> 0
-  AND @@trancount > 0
-  ROLLBACK TRANSACTION
-GO
-IF @@error = 0
-  PRINT 'Drop FK_GROUPCOU_FK_GC_COU_COUNTERK'
+  PRINT 'Drop FK_COUNTERS_COUNTERKIND'
 
 IF EXISTS (SELECT
       1
@@ -70,16 +52,16 @@ IF EXISTS (SELECT
       ON (o.id = r.constid
       AND o.type = 'F')
     WHERE r.fkeyid = OBJECT_ID('dbo.PSAttrsVal')
-    AND o.name = 'FK_PSATTRSV_FK_ATTRSR_ATTRSREF')
+    AND o.name = 'FK_PSATTRSVAL_ATTRREFID')
   ALTER TABLE dbo.PSAttrsVal
-  DROP CONSTRAINT FK_PSATTRSV_FK_ATTRSR_ATTRSREF
+  DROP CONSTRAINT FK_PSATTRSVAL_ATTRREFID
 GO
 IF @@error <> 0
   AND @@trancount > 0
   ROLLBACK TRANSACTION
 GO
 IF @@error = 0
-  PRINT 'Drop FK_PSATTRSV_FK_ATTRSR_ATTRSREF'
+  PRINT 'Drop FK_PSATTRSVAL_ATTRREFID'
 
 IF EXISTS (SELECT
       1
@@ -88,16 +70,16 @@ IF EXISTS (SELECT
       ON (o.id = r.constid
       AND o.type = 'F')
     WHERE r.fkeyid = OBJECT_ID('dbo.PSAttrsVal')
-    AND o.name = 'FK_PSATTRSV_FK_PRODUC_PRODUCTS')
+    AND o.name = 'FK_PSATTRSVAL_PRODUCTSITEID')
   ALTER TABLE dbo.PSAttrsVal
-  DROP CONSTRAINT FK_PSATTRSV_FK_PRODUC_PRODUCTS
+  DROP CONSTRAINT FK_PSATTRSVAL_PRODUCTSITEID
 GO
 IF @@error <> 0
   AND @@trancount > 0
   ROLLBACK TRANSACTION
 GO
 IF @@error = 0
-  PRINT 'Drop FK_PSATTRSV_FK_PRODUC_PRODUCTS'
+  PRINT 'Drop FK_PSATTRSVAL_PRODUCTSITEID'
 
 IF EXISTS (SELECT
       1
@@ -122,34 +104,16 @@ IF EXISTS (SELECT
       ON (o.id = r.constid
       AND o.type = 'F')
     WHERE r.fkeyid = OBJECT_ID('dbo.ProductSites')
-    AND o.name = 'FK_PRODUCTS_FK_PS_GRC_GROUPCOU')
+    AND o.name = 'FK_LOCATIONTYPEID')
   ALTER TABLE dbo.ProductSites
-  DROP CONSTRAINT FK_PRODUCTS_FK_PS_GRC_GROUPCOU
+  DROP CONSTRAINT FK_LOCATIONTYPEID
 GO
 IF @@error <> 0
   AND @@trancount > 0
   ROLLBACK TRANSACTION
 GO
 IF @@error = 0
-  PRINT 'Drop FK_PRODUCTS_FK_PS_GRC_GROUPCOU'
-
-IF EXISTS (SELECT
-      1
-    FROM sys.sysreferences r
-    JOIN sys.sysobjects o
-      ON (o.id = r.constid
-      AND o.type = 'F')
-    WHERE r.fkeyid = OBJECT_ID('dbo.Counters')
-    AND o.name = 'FK_COUNTERS_FK_COUNTE_PRODUCTS')
-  ALTER TABLE dbo.Counters
-  DROP CONSTRAINT FK_COUNTERS_FK_COUNTE_PRODUCTS
-GO
-IF @@error <> 0
-  AND @@trancount > 0
-  ROLLBACK TRANSACTION
-GO
-IF @@error = 0
-  PRINT 'Drop FK_COUNTERS_FK_COUNTE_PRODUCTS'
+  PRINT 'Drop FK_LOCATIONTYPEID'
 
 /*==============================================================*/
 /* Tables                                                       */
@@ -195,6 +159,25 @@ GO
 IF EXISTS (SELECT
       1
     FROM sysobjects
+    WHERE id = OBJECT_ID('dbo.LocationTypesRef')
+    AND type = 'U')
+  DROP TABLE dbo.LocationTypesRef
+GO
+
+/*==============================================================*/
+/* Table: LocationTypesRef                                      */
+/*==============================================================*/
+CREATE TABLE dbo.LocationTypesRef (
+  LocationTypeRefId SMALLINT NOT NULL
+ ,LocationTypeName VARCHAR(100) NOT NULL
+ ,CONSTRAINT PK_LOCATIONTYPES PRIMARY KEY (LocationTypeRefId)
+)
+GO
+
+
+IF EXISTS (SELECT
+      1
+    FROM sysobjects
     WHERE id = OBJECT_ID('dbo.Counters')
     AND type = 'U')
   DROP TABLE dbo.Counters
@@ -236,28 +219,8 @@ CREATE TABLE dbo.Counters (
   CounterId INT IDENTITY (1, 1) NOT FOR REPLICATION
  ,CounterName VARCHAR(50) COLLATE Cyrillic_General_CI_AS NOT NULL
  ,ConsumerId INT NULL
- ,ProductSiteId INT NULL
  ,CounterKindRefId SMALLINT NULL
  ,CONSTRAINT PK_COUNTERS PRIMARY KEY (CounterId)
-)
-GO
-
-IF EXISTS (SELECT
-      1
-    FROM sysobjects
-    WHERE id = OBJECT_ID('dbo.GroupCounters')
-    AND type = 'U')
-  DROP TABLE dbo.GroupCounters
-GO
-
-/*==============================================================*/
-/* Table: GroupCounters                                         */
-/*==============================================================*/
-CREATE TABLE dbo.GroupCounters (
-  GroupCounterId INT IDENTITY (1, 1) NOT FOR REPLICATION
- ,GroupCounterName VARCHAR(50) NOT NULL
- ,CounterKindRefId SMALLINT NULL
- ,CONSTRAINT PK_GROUPCOUNTERS PRIMARY KEY (GroupCounterId)
 )
 GO
 
@@ -273,12 +236,12 @@ GO
 /* Table: PSAttrsVal                                            */
 /*==============================================================*/
 CREATE TABLE dbo.PSAttrsVal (
-  ProductSiteAttrID INT NOT NULL
- ,ProductSiteId INT NULL
+  ProductSiteAttrID INT IDENTITY (1, 1) NOT FOR REPLICATION
+ ,ProductSiteId INT NOT NULL
  ,AttrRefId VARCHAR(20) NOT NULL
  ,ValueDate DATETIME NOT NULL
  ,SValue VARCHAR(100) NULL
- ,NValue NUMERIC
+ ,NValue NUMERIC(15, 2) NULL
  ,DValue DATETIME NULL
  ,CONSTRAINT PK_PSATTRSVAL PRIMARY KEY (ProductSiteAttrID)
 )
@@ -289,44 +252,38 @@ GO
 /*==============================================================*/
 CREATE UNIQUE INDEX UNQ_AttrID_Date ON dbo.PSAttrsVal (
 AttrRefId ASC,
-ValueDate ASC
+ValueDate ASC,
+ProductSiteId ASC
 )
 GO
 
-ALTER TABLE dbo.GroupCounters
-ADD CONSTRAINT FK_GROUPCOU_FK_GC_COU_COUNTERK FOREIGN KEY (CounterKindRefId)
-REFERENCES dbo.CounterKindsRef (CounterKindRefId)
-GO
+/*==============================================================*/
+/* Foreign Keys                                                 */
+/*==============================================================*/
 
 ALTER TABLE dbo.PSAttrsVal
-ADD CONSTRAINT FK_PSATTRSV_FK_ATTRSR_ATTRSREF FOREIGN KEY (AttrRefId)
+ADD CONSTRAINT FK_PSATTRSVAL_ATTRREFID FOREIGN KEY (AttrRefId)
 REFERENCES dbo.AttrsRef (AttrRefId)
 GO
 
 ALTER TABLE dbo.PSAttrsVal
-ADD CONSTRAINT FK_PSATTRSV_FK_PRODUC_PRODUCTS FOREIGN KEY (ProductSiteId)
+ADD CONSTRAINT FK_PSATTRSVAL_PRODUCTSITEID FOREIGN KEY (ProductSiteId)
 REFERENCES dbo.ProductSites (ProductSiteId)
 GO
 
-ALTER TABLE dbo.ProductSites
-ADD CONSTRAINT FK_PRODUCTS_FK_PS_GRC_GROUPCOU FOREIGN KEY (GroupCounterId)
-REFERENCES dbo.GroupCounters (GroupCounterId)
-GO
-
 ALTER TABLE dbo.Counters
-ADD CONSTRAINT FK_COUNTERS_FK_COUNTE_PRODUCTS FOREIGN KEY (ProductSiteId)
-REFERENCES dbo.ProductSites (ProductSiteId)
-GO
-
-
-ALTER TABLE dbo.Counters
-ADD CONSTRAINT FK_COUNTERS_FK_COUNTE_CONSUMER FOREIGN KEY (ConsumerId)
+ADD CONSTRAINT FK_COUNTERS_CONSUMERID FOREIGN KEY (ConsumerId)
 REFERENCES dbo.Consumers (ConsumerId)
 GO
 
 ALTER TABLE dbo.Counters
-ADD CONSTRAINT FK_COUNTERS_FK_COUNTE_COUNTERK FOREIGN KEY (CounterKindRefId)
+ADD CONSTRAINT FK_COUNTERS_COUNTERKIND FOREIGN KEY (CounterKindRefId)
 REFERENCES dbo.CounterKindsRef (CounterKindRefId)
+GO
+
+ALTER TABLE dbo.ProductSites
+ADD CONSTRAINT FK_LOCATIONTYPEID FOREIGN KEY (LocationTypeId)
+REFERENCES dbo.LocationTypesRef (LocationTypeRefId)
 GO
 
 COMMIT TRANSACTION
--- Model/forver_20170527.sql
+++ Model/forver_20170527.sql
@@ -1,39 +1,38 @@
 ﻿/* Заполнение таблиц */
+DELETE FROM dbo.PSAttrsVal
+GO
+DELETE FROM ProductSites
+GO
+DELETE FROM dbo.Counters
+GO
+DELETE FROM dbo.AttrsRef
+GO
+DELETE FROM dbo.LocationTypesRef
+GO
 DELETE FROM dbo.CounterKindsRef
 GO
+
 INSERT INTO dbo.CounterKindsRef (CounterKindRefId, CounterKindName)
   VALUES (1, 'Счетчик расхода тепловой энергии на отопление');
 INSERT INTO dbo.CounterKindsRef (CounterKindRefId, CounterKindName)
   VALUES (2, 'Счетчик расхода тепловой энергии на отопление и ГВС');
 INSERT INTO dbo.CounterKindsRef (CounterKindRefId, CounterKindName)
   VALUES (3, 'Счетчик расхода холодной воды');
-
-DELETE FROM dbo.AttrsRef
 GO
+
+INSERT INTO dbo.LocationTypesRef (LocationTypeRefId, LocationTypeName)
+  VALUES (1, 'Город');
+INSERT INTO dbo.LocationTypesRef (LocationTypeRefId, LocationTypeName)
+  VALUES (2, 'Село');
+GO
+
 INSERT INTO AttrsRef (AttrRefId, AttrFullName, AttrType)
   VALUES ('ENERGY_RATE', 'Норма расхода электроэнергии', 'N');
 INSERT INTO AttrsRef (AttrRefId, AttrFullName, AttrType)
   VALUES ('HEAT_LOSS_PERCENT', 'Процент тепловых потерь', 'N');
 GO
 
-DELETE FROM dbo.GroupCounters
-GO
-SET IDENTITY_INSERT GroupCounters OFF
-INSERT INTO dbo.GroupCounters (GroupCounterId, GroupCounterName, CounterKindRefId)
-  SELECT
-    p.kod
-   ,p.nazp
-   ,1
-  FROM dbo.pribor p
-  WHERE p.kod IN (SELECT
-      k.kodpr
-    FROM dbo.koteln k);
-SET IDENTITY_INSERT GroupCounters ON
-GO
-
-DELETE FROM ProductSites
-GO
-SET IDENTITY_INSERT ProductSites OFF
+SET IDENTITY_INSERT ProductSites ON
 INSERT INTO dbo.ProductSites (ProductSiteId, ProductSiteName, LocationTypeId, Chief)
   SELECT
     k.kodkot
@@ -41,6 +40,41 @@ INSERT INTO dbo.ProductSites (ProductSiteId, ProductSiteName, LocationTypeId, Ch
    ,k.mesto
    ,k.master
   FROM dbo.koteln k;
-SET IDENTITY_INSERT ProductSites ON
+SET IDENTITY_INSERT ProductSites OFF
 GO
 
+DECLARE @VDate DATE
+SELECT @VDate=p.start_period FROM dbo.paramorg p;
+INSERT INTO dbo.PSAttrsVal (ProductSiteId, AttrRefId, ValueDate, SValue, NValue, DValue)
+  SELECT
+    k.kodkot
+   ,'ENERGY_RATE'
+   ,@VDate
+   ,NULL
+   ,k.nel
+   ,NULL
+  FROM dbo.koteln k;
+INSERT INTO dbo.PSAttrsVal (ProductSiteId, AttrRefId, ValueDate, SValue, NValue, DValue)
+  SELECT
+    k.kodkot
+   ,'HEAT_LOSS_PERCENT'
+   ,@VDate
+   ,NULL
+   ,k.ps
+   ,NULL
+  FROM dbo.koteln k;
+GO
+
+SET IDENTITY_INSERT dbo.Counters ON
+GO
+INSERT INTO dbo.Counters (CounterId, CounterName, ConsumerId, CounterKindRefId)
+  SELECT
+    p.kod
+   ,p.nazp
+   ,p.kodorg
+   ,dbo.IIF(p.tgv, 0, 1, 2)
+  FROM dbo.pribor p;
+GO
+SET IDENTITY_INSERT dbo.Counters OFF
+GO
+PRINT 'ГОТОВО!!!'
